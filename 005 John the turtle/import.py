043a665c96681e943e0b4f86fa5978b9fb91d92f
Mariusz <98422358+SDA9@users.noreply.github.com>
2022-07-10 20:37:36 +0200

    Create import.py

---
+++ 005 John the turtle/import.py
@@ -0,0 +1,12 @@
+# import turtle
+# tom = turtle.Turtle()
+
+# from turtle import Turtle()
+# tom = Turtle()
+# john = Turtle()
+# marus = Turtle()
+
+# from turtle import *
+
+# import turtle as tr
+# john = tr.Turtle()
